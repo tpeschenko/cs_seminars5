5af286e4eeb82f885c938a405a087c3b7b1393e5
Taras <taras.peschenko.90@mail.ru>
2022-12-04 14:37:43 +0300

    Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+
+void Task34()
+{
+    int size = 4;
+
+    int[] numbers = new int[size];
+
+    FillArray(numbers);
+    PrintArray(numbers);
+    PrintEvenNumbers(numbers);
+    
+    void FillArray(int[] num)   
+    {
+        Random rand = new Random();
+        int LonArray = num.Length;
+        for(int i = 0; i < LonArray; i++)
+        {
+            num[i] = rand.Next(100, 1000);
+        }   
+    }
+
+    void PrintArray(int[] num)
+    {
+        int LonArray = num.Length;
+        Console.WriteLine("Вводный массив: ");
+        for(int i = 0; i < LonArray; i++)
+        {
+            Console.Write(num[i] + " ");
+        }
+        Console.WriteLine();
+    }   
+
+    void PrintEvenNumbers(int[] num)
+    {
+        Console.WriteLine("Чётные числа в массиве: ");
+        int LonArray = num.Length;
+        for(int i = 0; i < LonArray; i++)
+        {
+            if (num[i] % 2 == 0 )
+            {
+                Console.Write(num[i] + " ");
+            }
+ 
+        }
+        Console.WriteLine();
+    }
+}
+
+Task34();
+
